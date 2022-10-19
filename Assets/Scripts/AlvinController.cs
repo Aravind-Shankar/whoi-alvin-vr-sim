54b4b3df28cc67c2f93cc3a82c01f00fbdb68c07
anarayanan68 <anarayanan68@gatech.edu>
2022-10-19 12:42:04 -0400

    alvin controller adds constant hovering force (-mg)

--- Assets/Scripts/AlvinController.cs
+++ Assets/Scripts/AlvinController.cs
@@ -2,7 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class AlvinController : MonoBehaviour
 {
+    private Rigidbody mRigidbody;
 
+    private void Awake()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        AddHoveringForce();
+    }
+
+    private void AddHoveringForce()
+    {
+        mRigidbody.AddForce(-mRigidbody.mass * Physics.gravity);
+    }
 }
